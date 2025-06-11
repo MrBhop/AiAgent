253423eefb2250ec694c6a25a9bec1dec74336d0
Florian Schuett <flo.schuett@chello.at>
2025-06-11 11:06:57 +0200

    Add parameter for user prompt

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    if len(sys.argv) == 1:
+        print("AI Code Assistant")
+        print("\nUsage: python main.py <promp>")
+        sys.exit(1)
+    prompt = " ".join(sys.argv[1:])
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=prompt
     )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
