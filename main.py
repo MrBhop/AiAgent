45ddc7ebfb0a933e5cb25a0510c553a301038e79
Florian Schuett <flo.schuett@chello.at>
2025-06-11 11:12:09 +0200

    Add list for storing context

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     if len(sys.argv) == 1:
         print("AI Code Assistant")
         print("\nUsage: python main.py <promp>")
         sys.exit(1)
-    prompt = " ".join(sys.argv[1:])
+
+    user_prompt = " ".join(sys.argv[1:])
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages
     )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
